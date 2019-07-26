dde41147f676b807011c62ba00655c319be061ae
Viola Hu <lovelyviolaat@hotmail.com>
2019-07-26 12:32:19 +1000

    changed order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,12 +3,29 @@ class OrdersController < ApplicationController
   before_action :authenticate_user
 
   def create
+    # 1) create a new order, using the current_user's id
+    order = Order.create(user_id: current_user.id)
 
+    # 2) find the current_user's cart's line_items
     current_line_items = current_user.cart.line_items
 
+    # an array of each line_item object
+    current_line_items.each do |line_item|
+      # 3) change the line_item's cart_id to nil to stop the association with cart
+      line_item.cart_id = nil
+      # 4) meanwhile, attach the line_item to the new order
+      order.line_items << line_item
+    end
 
-    render json: current_line_items
+    # 5) also, need to deduct the purchased quantity of the product from its existing number
+    order.after_payment_change_product_stock
 
+    render json: current_line_items, include: :product
 
   end
+
+
+
+
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,14 @@ class Order < ApplicationRecord
   belongs_to :user, optional: true
   has_many :line_items, dependent: :destroy
   has_many :products, through: :line_items
+
+  def after_payment_change_product_stock
+    # deduct the purchased quantity of the line_item from its product's existing stock number
+    self.line_items.each do |line_item|
+      line_item.product.stock -= line_item.quantity
+    end
+  end
+
+
+
 end
